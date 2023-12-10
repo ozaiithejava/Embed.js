0fa3e60278678554cbe69a73e0ef18fafa116698
yigitthejava <82784013+ozaiithejava@users.noreply.github.com>
2023-12-10 15:10:41 +0300

    Update

---
+++ test.ts
@@ -0,0 +1,50 @@
+// main.ts
+
+import { DiscordEmbed, EmbedMessage } from './embed';
+
+// Replace 'YOUR_DISCORD_WEBHOOK_URL' with your actual Discord Webhook URL
+const webhookUrl = 'YOUR_DISCORD_WEBHOOK_URL';
+
+const embed = new DiscordEmbed(webhookUrl);
+
+// Süre sonra mesaj gönderme
+setTimeout(async () => {
+  await embed.sendEmbed({
+    title: 'Scheduled Embed',
+    description: 'This message was sent after a delay.',
+    color: 0x0000ff, // Blue color
+  });
+}, 5000); // 5 seconds delay
+
+// Döngü ile mesaj gönderme
+const loopCount = 3;
+for (let i = 0; i < loopCount; i++) {
+  await embed.sendEmbed({
+    title: 'Looped Embed',
+    description: `This is message number ${i + 1} in the loop.`,
+    color: 0xff00ff, // Magenta color
+  });
+}
+
+// Aynı mesajı birden çok kez gönderme
+const repeatCount = 4;
+await embed.sendRepeatedEmbed(
+  {
+    title: 'Repeated Embed',
+    description: 'This is the same message sent multiple times.',
+    color: 0xffff00, // Yellow color
+  },
+  repeatCount
+);
+
+// Çok detaylı mesaj gönderme
+const detailedMessage = new EmbedMessage(
+  'Detailed Embed',
+  'This is a detailed message with specific options.',
+  0x008000, // Green color
+  [
+    { name: 'Field 1', value: 'Value 1' },
+    { name: 'Field 2', value: 'Value 2' },
+  ]
+);
+await embed.sendDetailedEmbed(detailedMessage);
